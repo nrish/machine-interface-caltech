55cb92d1505d9a4a3753c6a325072c074488396b
fainc <farincorperated@gmail.com>
2021-07-24 03:16:39 -0700

    Switched the project to CMake and added bundling options. Removed old kDevelopment files and added a version header of the project, which could be used later.

---
+++ cmake/DeployQt.cmake
@@ -0,0 +1,82 @@
+find_package(Qt5Core REQUIRED)
+
+# Retrieve the absolute path to qmake and then use that path to find
+# the windeployqt and macdeployqt binaries
+get_target_property(_qmake_executable Qt5::qmake IMPORTED_LOCATION)
+get_filename_component(_qt_bin_dir "${_qmake_executable}" DIRECTORY)
+
+find_program(WINDEPLOYQT_EXECUTABLE windeployqt HINTS "${_qt_bin_dir}")
+if(WIN32 AND NOT WINDEPLOYQT_EXECUTABLE)
+    message(FATAL_ERROR " windeployqt not found")
+endif()
+
+find_program(MACDEPLOYQT_EXECUTABLE macdeployqt HINTS "${_qt_bin_dir}")
+if(APPLE AND NOT MACDEPLOYQT_EXECUTABLE)
+    message(FATAL_ERROR " macdeployqt not found")
+endif()
+
+find_program(LINUXDEPLOYQT_EXECUTABLE linuxdeployqt)
+if(UNIX AND NOT LINUXDEPLOYQT_EXECUTABLE)
+    message(WARNING " linuxdeployqt not found")
+    message("Be sure to pass in the correct path to executable with -DLINUXDEPLOYQT_EXECUTABLE")
+endif()
+
+# Add commands that copy the required Qt files to the same directory as the
+# target after being built as well as including them in final installation
+function(windeployqt target)
+
+    # Run windeployqt immediately after build
+    add_custom_command(TARGET ${target} POST_BUILD
+        COMMAND "${CMAKE_COMMAND}" -E
+            env PATH="${_qt_bin_dir}" "${WINDEPLOYQT_EXECUTABLE}"
+                --verbose 0
+                --no-compiler-runtime
+                --no-angle
+                --no-opengl-sw
+                --dir \"$<TARGET_FILE_DIR:${target}>/libs\"
+                \"$<TARGET_FILE:${target}>\"
+        COMMENT "Deploying Qt..."
+    )
+
+    # windeployqt doesn't work correctly with the system runtime libraries,
+    # so we fall back to one of CMake's own modules for copying them over
+
+    # Doing this with MSVC 2015 requires CMake 3.6+
+    if((MSVC_VERSION VERSION_EQUAL 1900 OR MSVC_VERSION VERSION_GREATER 1900)
+            AND CMAKE_VERSION VERSION_LESS "3.6")
+        message(WARNING "Deploying with MSVC 2015+ requires CMake 3.6+")
+    endif()
+
+    set(CMAKE_INSTALL_UCRT_LIBRARIES TRUE)
+    include(InstallRequiredSystemLibraries)
+    foreach(lib ${CMAKE_INSTALL_SYSTEM_RUNTIME_LIBS})
+        get_filename_component(filename "${lib}" NAME)
+        add_custom_command(TARGET ${target} POST_BUILD
+            COMMAND "${CMAKE_COMMAND}" -E
+                copy_if_different "${lib}" \"$<TARGET_FILE_DIR:${target}>/bundle\"
+            COMMENT "Copying ${filename}..."
+        )
+    endforeach()
+endfunction()
+
+# Add commands that copy the required Qt files to the application bundle
+# represented by the target.
+function(macdeployqt target)
+    add_custom_command(TARGET ${target} POST_BUILD
+        COMMAND "${MACDEPLOYQT_EXECUTABLE}"
+            \"$<TARGET_FILE_DIR:${target}>/bundle\"
+            -always-overwrite
+        COMMENT "Deploying Qt..."
+    )
+endfunction()
+
+function(linuxdeployqt target)
+    add_custom_command(TARGET ${target} POST_BUILD
+        COMMAND "${LINUXDEPLOYQT_EXECUTABLE}"
+            \"$<TARGET_FILE_DIR:${target}>/bundle\"
+            -always-overwrite
+        COMMENT "Deploying Qt..."
+    )
+endfunction()
+
+mark_as_advanced(WINDEPLOYQT_EXECUTABLE MACDEPLOYQT_EXECUTABLE LINUXDEPLOYQT_EXECUTABLE)
